8a4d39fa4705fc8c97457c9b8f47008afbc6430a
KirillSpitsyn <72778161+KirillSpitsyn@users.noreply.github.com>
2023-09-25 01:37:34 -0400

    Update Lab2.sol

--- Lab2.sol
+++ Lab2.sol
@@ -1,46 +1,43 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
-contract CallingContract {
-    string public message = "Something";
+contract Sender {
+    //state varibles to hold description and id of the sender and receiver, state varibles must be the same for 
+    //both receiver and sender to make delegateCalls working
+    string public description="Test description";
+    uint public id=15;
 
-    address public owner = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    //function to set description of sender and receiver 
+    function setDescription(string memory _description) external{
+        description=_description;
+    }    
 
-    function set(address _calledAddress) external {
-        (bool success, bytes memory returndata) = _calledAddress.delegatecall(
-            abi.encodeWithSelector(
-                TargetContract.changeMessage.selector,
-                "He is Alive"
-            )
-        );
-
-        // if the function call reverted
-        if (success == false) {
-            // if there is a return reason string
-            if (returndata.length > 0) {
-                // bubble up any reason for revert
-                assembly {
-                    let returndata_size := mload(returndata)
-                    revert(add(32, returndata), returndata_size)
-                }
-            } else {
-                revert("Function call reverted");
-            }
-        }
-    }
+    //function to set id of sender and receiver 
+    function setID(uint _id) external{
+        id=_id;
+    } 
 }
 
-contract TargetContract {
-    string public message = "I love solidity";
-    address public owner; //0x637CcDeBB20f849C0AA1654DEe62B552a058EA87
+contract Receiver {
+    //the same state varibles with Sender
+    string public description;
+    uint public id;
 
-    constructor(address _owner) {
-        owner = _owner;
-    }
 
-    function changeMessage(string calldata _msg) external {
-        message = _msg;
-        owner = msg.sender;
-    }
+    //creates a delegatecall to setDescription function in sender contract to set description value 
+    //for receiver without declaring setDescription function in Receiver contract
+    function setDescriptionUsingSender(address _contract, string calldata _description) external{
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSelector(Sender.setDescription.selector, _description));
+            require(success, "Calling delegateCall failed");
+    }  
+
+     //creates a delegatecall to setID function in sender contract to set ID value 
+     //for receiver without declaring setID function in Receiver contract
+     function setIDUsingSender(address _contract, uint _id) external{
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSelector(Sender.setID.selector, _id));
+            require(success, "Calling delegateCall failed");
+    }  
 
 }
