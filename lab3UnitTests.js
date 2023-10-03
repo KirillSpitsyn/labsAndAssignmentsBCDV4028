5603ae7933fbdea9063d14b75a16d38f2b24559f
KirillSpitsyn <72778161+KirillSpitsyn@users.noreply.github.com>
2023-10-03 16:30:40 -0400

    Update lab3UnitTests.js

--- lab3UnitTests.js
+++ lab3UnitTests.js
@@ -1,133 +1,161 @@
 const Lending = artifacts.require("Lending");
 
 contract("Lending", (accounts) => {
-    let LendingInstance;
-    const borrower = accounts[0];
-    const lender = accounts[1];
+    const owner = accounts[0];
+    const borrower = accounts[1];
+    const lender = accounts[2];
+    let lendingContract;
 
-    beforeEach(async () => {
-        LendingInstance = await Lending.new();
+    before(async () => {
+        lendingContract = await Lending.new({ from: owner });
     });
 
-    it("should create a loan and emit LoanCreated event", async () => {
-        const amount = web3.utils.toWei("1", "ether");
-        const interest = 5;
-        const duration = 30;
+    describe("createLoan", () => {
+        it("should create a new loan", async () => {
+            const amount = web3.utils.toWei("1", "ether");
+            const interest = 10;
+            const duration = 30;
 
-        const { logs } = await LendingInstance.createLoan(amount, interest, duration, { from: borrower });
-        const loanInfo = await pLendingInstance.getLoanInfo(0);
+            const tx = await lendingContract.createLoan(amount, interest, duration, { from: borrower });
 
-        assert.equal(loanInfo.amount.toString(), amount.toString(), "Incorrect loan amount");
-        assert.equal(loanInfo.interest.toNumber(), interest, "Incorrect interest rate");
-        assert.equal(loanInfo.duration.toNumber(), duration, "Incorrect loan duration");
-        assert.equal(loanInfo.borrower, borrower, "Incorrect borrower address");
-        assert.equal(loanInfo.lender, "0x0000000000000000000000000000000000000000", "Lender should be uninitialized");
-        assert.equal(loanInfo.active, true, "Loan should be active");
-        assert.equal(loanInfo.repaid, false, "Loan should not be repaid");
+            // Check the LoanCreated event
+            assert.equal(tx.logs[0].event, "LoanCreated");
+            assert.equal(tx.logs[0].args.borrower, borrower);
+        });
 
-        const log = logs[0];
-        assert.equal(log.event, "LoanCreated", "Event not emitted");
-        assert.equal(log.args.amount.toString(), amount.toString(), "Incorrect event amount");
-        assert.equal(log.args.interest.toNumber(), interest, "Incorrect event interest rate");
-        assert.equal(log.args.duration.toNumber(), duration, "Incorrect event duration");
-        assert.equal(log.args.borrower, borrower, "Incorrect event borrower address");
-        assert.equal(log.args.lender, "0x0000000000000000000000000000000000000000", "Event lender should be uninitialized");
+        it("should not create a loan with amount below 0.1 ETH", async () => {
+            const amount = web3.utils.toWei("0.08", "ether"); // Below the MIN_LOAN_AMOUNT
+
+            try {
+                await lendingContract.createLoan(amount, 10, 30, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "Loan amount must be between");
+            }
+        });
+
+        it("should not create a loan with amount above 25 ETH", async () => {
+            const amount = web3.utils.toWei("26", "ether"); // Above the MAX_LOAN_AMOUNT
+
+            try {
+                await lendingContract.createLoan(amount, 10, 30, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "Loan amount must be between");
+            }
+        });
+
+        it("should not create a loan with interest rate below 10% interest", async () => {
+            const interest = 7; // Below the MIN_INTEREST_RATE
+
+            try {
+                await lendingContract.createLoan(web3.utils.toWei("1", "ether"), interest, 30, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "Interest rate must be between");
+            }
+        });
+
+        it("should not create a loan with interest rate above 50% interest rate", async () => {
+            const interest = 55; // Above the MAX_INTEREST_RATE
+
+            try {
+                await lendingContract.createLoan(web3.utils.toWei("1", "ether"), interest, 30, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "Interest rate must be between");
+            }
+        });
+
+        it("should not create a loan with zero duration", async () => {
+            try {
+                await lendingContract.createLoan(web3.utils.toWei("1", "ether"), 10, 0, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "Loan duration must be greater than 0");
+            }
+        });
     });
 
-    it("should fund a loan and emit LoanFunded event", async () => {
-        const amount = web3.utils.toWei("1", "ether");
-        const loanId = 0;
+    describe("fundLoan", () => {
+        it("should fund your loan fully", async () => {
+            const loanId = 0;
+            const amount = web3.utils.toWei("0.1", "ether");
+            const tx = await lendingContract.fundLoan(loanId, { from: lender, value: amount });
 
-        await LendingInstance.createLoan(amount, 5, 30, { from: borrower });
-        const { logs } = await LendingInstance.fundLoan(loanId, { from: lender, value: amount });
+            // Check the LoanFunded event
+            assert.equal(tx.logs[0].event, "LoanFunded");
+            assert.equal(tx.logs[0].args.funder, lender);
+        });
 
-        const loanInfo = await LendingInstance.getLoanInfo(loanId);
+        it("should not fund a loan with insufficient amount", async () => {
+            const loanId = 0;
+            const amount = web3.utils.toWei("0.09", "ether"); // Below the loan amount
 
-        assert.equal(loanInfo.lender, lender, "Incorrect lender address");
-        assert.equal(loanInfo.active, false, "Loan should not be active after funding");
-
-        const log = logs[0];
-        assert.equal(log.event, "LoanFunded", "Event not emitted");
-        assert.equal(log.args.loanId.toNumber(), loanId, "Incorrect event loanId");
-        assert.equal(log.args.funder, lender, "Incorrect event funder address");
-        assert.equal(log.args.amount.toString(), amount.toString(), "Incorrect event amount");
+            try {
+                await lendingContract.fundLoan(loanId, { from: lender, value: amount });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "not enough");
+            }
+        });
     });
 
-    it("should repay a loan and emit LoanRepaid event", async () => {
-        const amount = web3.utils.toWei("1", "ether");
-        const loanId = 0;
+    describe("repayLoan", () => {
+        it("should repay a loan", async () => {
+            const loanId = 0;
+            const repayAmount = web3.utils.toWei("0.1", "ether");
+            const tx = await lendingContract.repayLoan(loanId, repayAmount, { from: borrower });
 
-        await LendingInstance.createLoan(amount, 5, 30, { from: borrower });
-        await LendingInstance.fundLoan(loanId, { from: lender, value: amount });
+            // Check the LoanRepaid event
+            assert.equal(tx.logs[0].event, "LoanRepaid");
+            assert.equal(tx.logs[0].args.amount.toString(), repayAmount);
+        });
 
-        const { logs } = await LendingInstance.repayLoan(loanId, { from: borrower, value: amount });
+        it("should not repay a loan with incorrect repayment amount", async () => {
+            const loanId = 0;
+            const repayAmount = web3.utils.toWei("0.09", "ether"); // Incorrect amount
 
-        const loanInfo = await LendingInstance.getLoanInfo(loanId);
-
-        assert.equal(loanInfo.repaid, true, "Loan should be marked as repaid");
-        assert.equal(loanInfo.active, false, "Loan should not be active after repayment");
-
-        const log = logs[0];
-        assert.equal(log.event, "LoanRepaid", "Event not emitted");
-        assert.equal(log.args.loanId.toNumber(), loanId, "Incorrect event loanId");
-        assert.equal(log.args.amount.toString(), amount.toString(), "Incorrect event amount");
+            try {
+                await lendingContract.repayLoan(loanId, repayAmount, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "Incorrect repayment amount");
+            }
+        });
     });
 
-    it("should not allow a borrower to fund their own loan", async () => {
-        const amount = web3.utils.toWei("1", "ether");
+    describe("getLoanInfo", () => {
+        it("should get information about requested loan", async () => {
+            const loanId = 0;
+            const loanInfo = await lendingContract.getLoanInfo(loanId);
 
-        await LendingInstance.createLoan(amount, 5, 30, { from: borrower });
-
-        try {
-            await LendingInstance.fundLoan(0, { from: borrower, value: amount });
-            assert.fail("Borrower should not be able to fund their own loan");
-        } catch (error) {
-            assert.include(error.message, "Borrower cannot fund their own loan");
-        }
+            assert.notEqual(loanInfo.amount, 0);
+            assert.notEqual(loanInfo.interest, 0);
+        });
     });
 
-    it("should not allow funding after the funding deadline", async () => {
-        const amount = web3.utils.toWei("1", "ether");
+    describe("withdrawFunds", () => {
+        it("should withdraw funds from loan to your account", async () => {
+            const loanId = 0;
+            const tx = await lendingContract.withdrawFunds(loanId, { from: borrower });
 
-        await LendingInstance.createLoan(amount, 5, 1, { from: borrower });
+            // Check that funds were withdrawn
+            assert.notEqual(tx.receipt.gasUsed, 0);
+        });
 
-        // Wait for the funding deadline to pass
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        it("should not withdraw funds from an inactive loan", async () => {
+            const loanId = 0;
 
-        try {
-            await LendingInstance.fundLoan(1, { from: lender, value: amount });
-            assert.fail("Funding should not be allowed after the funding deadline");
-        } catch (error) {
-            assert.include(error.message, "Loan funding deadline has passed");
-        }
+            // Deactivate the loan
+            await lendingContract.withdrawFunds(loanId, { from: borrower });
+
+            try {
+                await lendingContract.withdrawFunds(loanId, { from: borrower });
+                assert.fail("Expected an error but succeeded");
+            } catch (error) {
+                assert.include(error.message, "reverted with reason");
+            }
+        });
     });
-
-    it("should not allow repayment of incorrect amount", async () => {
-        const amount = web3.utils.toWei("1", "ether");
-
-        await LendingInstance.createLoan(amount, 5, 30, { from: borrower });
-        await LendingInstance.fundLoan(0, { from: lender, value: amount });
-
-        try {
-            await LendingInstance.repayLoan(0, { from: borrower, value: web3.utils.toWei("1.10", "ether") });
-            assert.fail("Repayment should not be allowed with incorrect amount");
-        } catch (error) {
-            assert.include(error.message, "Incorrect repayment amount");
-        }
-    });
-
-    it("should allow a borrower to withdraw remaining funds after loan is not active", async () => {
-        const amount = web3.utils.toWei("1", "ether");
-        const loanId = 0;
-
-        await LendingInstance.createLoan(amount, 5, 30, { from: borrower });
-        await LendingInstance.fundLoan(loanId, { from: lender, value: amount });
-        await LendingInstance.repayLoan(loanId, { from: borrower, value: amount });
-
-        const balanceBeforeWithdrawal = await web3.eth.getBalance(borrower);
-
-        await LendingInstance.withdrawFunds(loanId, { from: borrower });
-
-        const balanceAfterWithdrawal = await web3.eth.getBalance(borrower);
-
-        assert.isTrue(balanceAfterWithdrawal > balanceBefore
+});
